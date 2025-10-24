0036001bc98476a9863f8b2f5a7187b686edd93a
svensnaterse-sudo <svensnaterse@gmail.com>
2025-10-24 19:12:35 +0200

    Fix file names for loading model and scaler

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ Adjust the sliders in the sidebar to set input features, then see your predicted
 # ----------------------------
 @st.cache_resource
 def load_model_and_data():
-    knn_model = load("knn_model.joblib (1)")
-    scaler = load("scaler.joblib (1)")
+    knn_model = load("knn_model.joblib")
+    scaler = load("scaler.joblib")
     X_train = load("X_train.joblib")
     y_train = load("y_train.joblib")
     return knn_model, scaler, X_train, y_train
