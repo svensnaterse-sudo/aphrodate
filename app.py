ebfcb3359dee0d3d526a84c5c0d5a16422ab3f96
svensnaterse-sudo <svensnaterse@gmail.com>
2025-10-24 18:38:27 +0200

    Add race features to feature_columns in app.py

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ knn_model, scaler = load_model()
 # ----------------------------
 feature_columns = [
     "age", "attractive", "intelligence", "funny", "ambition", "ambtition_important", "art", "attractive_important", "clubbing", "concerts", "d_age", "dining", "exercise",
-    "gaming", "gender_male", "hiking", "importance_same_race", "intellicence_important", "interests_correlate", "movies", "musuem", "music"
+    "gaming", "gender_male", "hiking", "importance_same_race", "intellicence_important", "interests_correlate", "movies", "musuems", "music", "race_Black/African American",
+    "race_European/Caucasian-American", "race_Latino/Hispanic American", "race_Other"
     # Add more features if your dataset has them
 ]
 
